6338634389bba4804d4be5d8804da9867e0cfeb7
Anu Sethi <anu1301@hotmail.co.uk>
2022-03-14 20:03:08 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+def print_board(board):
+    """
+    This function prints the board out, which consists of a list of
+    10 strings representing the cells in the board (ignore index 0)
+    """
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
