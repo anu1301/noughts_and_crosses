b1ad51ab039ecd1afcb4473ba16224738384ae79
Anu Sethi <anu1301@hotmail.co.uk>
2022-03-14 20:20:51 +0000

    Add player choice of 'X' or 'O'

--- run.py
+++ run.py
@@ -17,3 +17,21 @@ def print_board(board):
     print('   |   |')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
+
+
+def input_letter_choice():
+    """
+    Lets the player choose X or O.
+    Returns as list with the player's choice as first item, 
+    and the computer's letter as the second.
+    """
+    letter = ''
+    while not (letter == 'X' or letter == 'O'):
+        print('Do you want to be X or O?')
+        letter = input().upper()
+
+    if letter == 'X':
+        # First character is player's letter, and second the computers
+        return ['X', 'O']  
+    else:
+        return ['O', 'X']
